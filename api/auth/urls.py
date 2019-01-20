c9d061aa6bdb40e17fe24d6e764e985df8852d8b
Wojciech Kowasz <wojciech@kowasz.pl>
2019-01-20 12:38:24 +0100

    Minor changes in autentication module
    * Added AllowAnonymousOnly custom permission for login and register views
    * Changed login endpoint to /auth/login/

--- api/auth/urls.py
+++ api/auth/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
 from .views import LoginView, RegisterView
 
 urlpatterns = [
-    path('', LoginView.as_view()),
+    path('login/', LoginView.as_view()),
     path('register/', RegisterView.as_view()),
 ]
