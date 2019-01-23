e2e6264e0381bb4e1181d7c20226269fd679ff79
Wojciech Kowasz <wojciech@kowasz.pl>
2019-01-23 09:56:20 +0100

    Further improvements in Letter view and serializer * status attribute validation * soft-deletion support

--- api/letters/serializers.py
+++ api/letters/serializers.py
@@ -24,3 +24,27 @@ class LetterSerializer(serializers.ModelSerializer):
         if len(value) > 280:
             raise serializers.ValidationError("Content is too long.")
         return value
+
+    def validate_status(self, value):
+        #request = self.context["request"]
+        new_status = value
+        prev_status = self.instance.status
+
+        # status validation depending on current letter state
+        if prev_status is not None and new_status is not None:
+            if prev_status == Letter.DRAFT_STATUS:
+                if new_status not in [Letter.DRAFT_STATUS, Letter.SENT_STATUS]:
+                    raise serializers.ValidationError(
+                        "Selected status is not allowed in current object state")
+            elif prev_status in [Letter.SENT_STATUS, Letter.READ_STATUS]:
+                if new_status != [Letter.READ_STATUS]:
+                    raise serializers.ValidationError(
+                        "Selected status is not allowed in current object state")
+
+        elif new_status is not None:
+            # new item is being created
+            if new_status not in [Letter.DRAFT_STATUS, Letter.SENT_STATUS]:
+                raise serializers.ValidationError(
+                        "Selected status is not allowed in current object state")
+
+        return value
--- api/letters/views.py
+++ api/letters/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.utils import timezone
 from rest_framework import generics
 
 from .models import Letter
@@ -10,6 +11,8 @@ class LettersView(generics.ListCreateAPIView):
     serializer_class = LetterSerializer
 
     def get_queryset(self):
+        # todo: reuse this qs
+        
         user = self.request.user
         # return only those letters where:
         # - user is a sender and letter is not deleted
@@ -39,5 +42,38 @@ class LetterItemView(generics.RetrieveUpdateDestroyAPIView):
     ]
 
     serializer_class = LetterSerializer
-    queryset = Letter.objects.all()
     lookup_field = 'id'
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def get_queryset(self):
+        # todo: reuse this qs
+
+        user = self.request.user
+        # return only those letters where:
+        # - user is a sender and letter is not deleted
+        # - user is a recipient, letter is not unsent and is not delted
+        return Letter.objects.filter(
+            (
+                Q(sender=user) &
+                Q(sender_deleted=False)
+            ) |
+            (
+                Q(recipient=user) &
+                Q(recipient_deleted=False) &
+                Q(status__in=[Letter.SENT_STATUS, Letter.READ_STATUS])
+            )
+        )
+
+    def perform_destroy(self, instance):
+        if self.request.user == instance.sender:
+            obj = Letter.objects.get(id=instance.id)
+            obj.sender_deleted = True
+            obj.sender_deleted_on = timezone.now()
+            obj.save()
+        elif self.request.user == instance.recipient:
+            obj = Letter.objects.get(id=instance.id)
+            obj.recipient_deleted = True
+            obj.recipient_deleted_on = timezone.now()
+            obj.save()
