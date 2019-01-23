4d18c59d79f83e8adae3e499c88a88ef234bbba4
Wojciech Kowasz <wojciech@kowasz.pl>
2019-01-23 12:01:33 +0100

    Fix for missing migrations command in docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install -r requirements.txt
-python makemigrations
-python migrate
+python manage.py makemigrations
+python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
