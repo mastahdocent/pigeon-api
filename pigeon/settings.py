2c58b95712d646e4c16c221fe0b60596da6cf7a3
Wojciech Kowasz <wojciech@kowasz.pl>
2019-01-23 14:13:02 +0100

    Database backend switched to MySQL

--- pigeon/settings.py
+++ pigeon/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'pigeon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pigeon',
+        'USER': 'pigeon',
+        'PASSWORD': '##pigeon1', # for testing purposes only
+        'HOST': 'dev03x.xenium.pl',   
+        'PORT': '3306',
     }
 }
 
