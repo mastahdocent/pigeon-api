923a79d85935b46c8d43fb2d9bdf8e489e03bd24
Wojciech Kowasz <wojciech@kowasz.pl>
2019-01-26 21:25:33 +0100

    User avatar upload

--- api/models.py
+++ api/models.py
@@ -1,9 +1,19 @@
+import os
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# todo: overwrite avatars instead of adding suffixes to another filename
+
+def avatars_path(instance, filename):
+    file_extension = os.path.splitext(filename)[-1]
+    new_filename = "{id}{ext}".format(id=instance.id, ext=file_extension)
+    return "images/avatars/{filename}".format(filename=new_filename)
+
+
 class User(AbstractUser):
     email = models.EmailField(blank=False, null=False, unique=True)
-    avatar = models.ImageField(blank=True, null=True)
+    avatar = models.ImageField(blank=True, null=True, upload_to=avatars_path)
 
     def __str__(self):
         return self.username
